9009764083af48a81d91a82ae2e5e75b03ebb82a
sujianhui <jianhui.su@bianli24.com>
2019-03-20 21:10:31 +0800

    03-20   commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import com.jianhui.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Goods goods = new Goods();
+        int result = goods.finish(9);
+
+        System.out.println(result);
+    }
+}
---
+++ src/com/jianhui/Goods.java
@@ -0,0 +1,10 @@
+package com.jianhui;
+
+public class Goods {
+    public int finish(int count)
+    {
+        count++;
+
+        return  count;
+    }
+}
